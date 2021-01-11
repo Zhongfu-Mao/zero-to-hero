864456f2526ca3cbca2fb63f24ee163e757ae623
Mao ZhongFu <34616854+Zhongfu-Mao@users.noreply.github.com>
2021-01-11 15:41:21 +0900

    添加pytest第一部分

---
+++ 自動化/テスト/pytest/01.Getting Start/test_three.py
@@ -0,0 +1,20 @@
+""" Test the Task data type """
+
+from collections import namedtuple
+
+
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task.__new__.__defaults__ = (None, None, False, None)
+
+
+def test_defaults():
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+
+def test_member_access():
+    t = Task('buy milk', 'mao')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'mao'
+    assert (t.done, t.id) == (False, None)
