3ca6908e234e04572cbce9f637debb092c2b0f7a
Mao ZhongFu <34616854+Zhongfu-Mao@users.noreply.github.com>
2021-01-15 00:08:57 +0900

    修复错误

--- DataScience/機械学習/algorithm/線形回帰/01-Simple-Linear-Regression-Implementation/playML/SimpleLinearRegression.py
+++ DataScience/機械学習/algorithm/線形回帰/01-Simple-Linear-Regression-Implementation/playML/SimpleLinearRegression.py
@@ -4,12 +4,10 @@ import numpy as np
 class SimpleLinearRegression1:
 
     def __init__(self):
-        """初始化Simple Linear Regression 模型"""
         self.a_ = None
         self.b_ = None
 
     def fit(self, x_train, y_train):
-        """根据训练数据集x_train,y_train训练Simple Linear Regression模型"""
         assert x_train.ndim == 1, \
             "Simple Linear Regressor can only solve single feature training data."
         assert len(x_train) == len(y_train), \
@@ -30,7 +28,6 @@ class SimpleLinearRegression1:
         return self
 
     def predict(self, x_predict):
-        """给定待预测数据集x_predict，返回表示x_predict的结果向量"""
         assert x_predict.ndim == 1, \
             "Simple Linear Regressor can only solve single feature training data."
         assert self.a_ is not None and self.b_ is not None, \
@@ -39,7 +36,6 @@ class SimpleLinearRegression1:
         return np.array([self._predict(x) for x in x_predict])
 
     def _predict(self, x_single):
-        """给定单个待预测数据x，返回x的预测结果值"""
         return self.a_ * x_single + self.b_
 
     def __repr__(self):
@@ -49,12 +45,10 @@ class SimpleLinearRegression1:
 class SimpleLinearRegression2:
 
     def __init__(self):
-        """初始化Simple Linear Regression模型"""
         self.a_ = None
         self.b_ = None
 
     def fit(self, x_train, y_train):
-        """根据训练数据集x_train,y_train训练Simple Linear Regression模型"""
         assert x_train.ndim == 1, \
             "Simple Linear Regressor can only solve single feature training data."
         assert len(x_train) == len(y_train), \
@@ -69,7 +63,6 @@ class SimpleLinearRegression2:
         return self
 
     def predict(self, x_predict):
-        """给定待预测数据集x_predict，返回表示x_predict的结果向量"""
         assert x_predict.ndim == 1, \
             "Simple Linear Regressor can only solve single feature training data."
         assert self.a_ is not None and self.b_ is not None, \
@@ -78,7 +71,6 @@ class SimpleLinearRegression2:
         return np.array([self._predict(x) for x in x_predict])
 
     def _predict(self, x_single):
-        """给定单个待预测数据x_single，返回x_single的预测结果值"""
         return self.a_ * x_single + self.b_
 
     def __repr__(self):
